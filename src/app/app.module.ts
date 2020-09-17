032e4f36df2c961ae6fe0abef12827fcadfdc802
pedroleaop <pedroleaop@hotmail.com>
2020-09-16 21:03:47 -0300

    alterações para o single-card de modelo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './components/navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { AdressFormComponent } from './components/adress-form/adress-form.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
+/* components */
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { AdressFormComponent } from './components/adress-form/adress-form.component';
 import { SingleCardComponent } from './components/single-card/single-card.component';
 import { AllCardsComponent } from './components/all-cards/all-cards.component';
+
+
 
 @NgModule({
   declarations: [
--- src/app/components/single-card/single-card.component.ts
+++ src/app/components/single-card/single-card.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleCardComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  title: string = 'Casa de praia em Itamaracá';
+  owner: string = 'José João';
+  imgURL: string = 'https://material.angular.io/assets/img/examples/shiba1.jpg';
+  description: string = 'Casa de praia em Itamaracá super confortável para até 20 pessoas.';
 
 }
